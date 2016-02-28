9eea3eb5fab63ad6ecd51d07017bcc0fc7a794d6
Scott Richards <scott.srichards@gmail.com>
2016-02-27 18:41:19 -0800

    Working on Page Adapter

--- app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
@@ -1,8 +1,12 @@
 package com.codepath.apps.mysimpletweets.activities;
 
+import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -15,6 +19,8 @@ import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.adapters.TweetsArrayAdapter;
 import com.codepath.apps.mysimpletweets.TwitterApplication;
 import com.codepath.apps.mysimpletweets.TwitterClient;
+import com.codepath.apps.mysimpletweets.fragments.HomeTimelineFragment;
+import com.codepath.apps.mysimpletweets.fragments.MentionsTimelineFragment;
 import com.codepath.apps.mysimpletweets.fragments.TweetsListFragment;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.codepath.apps.mysimpletweets.utils.EndlessScrollListener;
@@ -37,15 +43,18 @@ public class TimelineActivity extends AppCompatActivity {
 //        pb = (ProgressBar) findViewById(R.id.pbLoading);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TweetsPagerAdapater(getSupportFragmentManager()));
 //        client = TwitterApplication.getRestClient();
 //        lvTweets = (ListView)findViewById(R.id.lvTweets);
 //        tweets = new ArrayList<Tweet>();
 //        aTweets = new TweetsArrayAdapter(this, tweets);
 //        lvTweets.setAdapter(aTweets);
 //        populateTimeline();
-        if (savedInstanceState == null) {
-            fragmentTweetsList = (TweetsListFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTweetList);
-        }
+//        if (savedInstanceState == null) {
+//            fragmentTweetsList = (TweetsListFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentTweetList);
+//        }
 //        setupScrolling();
         // on some click or some loading we need to wait for...
     }
@@ -69,7 +78,7 @@ public class TimelineActivity extends AppCompatActivity {
         // This method probably sends out a network request and appends new data items to your adapter.
         // Use the offset value and add it as a parameter to your API request to retrieve paginated data.
         // Deserialize API response and then construct new objects to append to the adapter
-        Log.d("DEBUG","do something here");
+        Log.d("DEBUG", "do something here");
 //        populateTimeline();
     }
 
@@ -89,6 +98,33 @@ public class TimelineActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public class TweetsPagerAdapater extends FragmentPagerAdapter {
+        private String tabTitles[] = {"Home", "Mentions"};
+
+        public TweetsPagerAdapater(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new HomeTimelineFragment();
+            } else if (position == 1) {
+                return new MentionsTimelineFragment();
+            }
+            return null;
+        }
+
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+
+        // return number of tabs
+        @Override
+        public int getCount() {
+            return tabTitles.length;
+        }
+    }
 
 
 //    // after adding tweets look up the min id
