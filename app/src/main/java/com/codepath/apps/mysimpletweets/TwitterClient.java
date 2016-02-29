e0893b208972adb9db37dcd8e4b90fe91011f131
Scott Richards <scott.srichards@gmail.com>
2016-02-28 16:03:38 -0800

    Clicking on userName displays corresponding user info

--- app/src/main/java/com/codepath/apps/mysimpletweets/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TwitterClient.java
@@ -95,6 +95,15 @@ public class TwitterClient extends OAuthBaseClient {
 		getClient().get(apiUrl, null, handler);
 	}
 
+	public void getUserInfo(String screenName, AsyncHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("users/show.json");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("screen_name",screenName);
+		Log.d("DEBUG", "Making request to apiUrl: " + apiUrl);
+		getClient().get(apiUrl, params, handler);
+	}
+
 	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
 	 * 	  i.e getApiUrl("statuses/home_timeline.json");
 	 * 2. Define the parameters to pass to the request (query or body)
